a7b19b8a52a369ec6f3f77c248246d43326ec821
Ronaldo Rodrigues Godoi <anyblueangel3@gmail.com>
2024-08-30 20:55:39 -0300

    Ajuste de digitação.

--- src/Contador.java
+++ src/Contador.java
@@ -6,6 +6,7 @@ class ParametrosInvalidosException extends Exception {
 	public ParametrosInvalidosException(String message) {
 			super(message);
 	}
+
 }
 
 public class Contador {
@@ -27,7 +28,8 @@ public class Contador {
 	}
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
-		if(parametroUm >= parametroDois) throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro.");
+		if(parametroUm >= parametroDois)
+			throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro.");
 		
 		int contagem = parametroDois - parametroUm;
 		//realizar o for para imprimir os números com base na variável contagem
